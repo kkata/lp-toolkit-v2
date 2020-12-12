8e17c3d60aead49956d189e3eecfef712bdf8dd3
kkata <14849535+kkata@users.noreply.github.com>
2020-12-12 11:38:22 +0900

    text in value

--- src/js/hoge/controllers/toggle_controller.js
+++ src/js/hoge/controllers/toggle_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static values = { isOpen: Boolean }
+  static values = { isOpen: Boolean, close: String, more: String }
   static targets = ['content']
   static classes = ['visible']
 
@@ -15,6 +15,8 @@ export default class extends Controller {
 
   toggleText(e) {
     this.isOpen = !this.isOpen
-    return (e.currentTarget.innerHTML = this.isOpen ? '閉じる' : '続きを読む')
+    return (e.currentTarget.innerHTML = this.isOpen
+      ? this.closeValue
+      : this.moreValue)
   }
 }
